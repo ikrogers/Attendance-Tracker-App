2666ac91722cfa50a038550ceaabded2ad818ca9
Ilya Rogers <ikrogers92@gmail.com>
2014-07-22 19:32:13 -0400

    h

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ devise_for :users, :controllers => { :registrations => "registrations" }
 devise_scope :user do
   authenticated :user do
     
-    root :to => 'homepage#index', as: :authenticated_root
+    root :to => 'homepage#index'
     end
  
   unauthenticated :user do
