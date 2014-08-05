b0b24e6e5ca1a8d8653bc54d68ba72e0c473cfd3
Ilya Rogers <ikrogers92@gmail.com>
2014-08-05 02:58:39 -0400

    Confirm

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,3 +1,16 @@
 class ConfirmationsController < Devise::ConfirmationsController
   layout 'application'
+  
+  # GET /resource/confirmation?confirmation_token=abcdef
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message(:notice, :confirmed) if is_flashing_format?
+      respond_with_navigational(resource){ redirect_to unauthenticated_root_path }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+  end
 end
