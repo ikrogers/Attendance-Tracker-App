8b92d097fe05cc2880abb09d65d072bd0bff8425
Ilya Rogers <ikrogers92@gmail.com>
2014-08-05 04:44:45 -0400

    confirms

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,26 @@
 class ConfirmationsController < Devise::ConfirmationsController
   layout 'application'
   
+   # POST /resource/confirmation
+  def create
+    self.resource = resource_class.send_confirmation_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      if mobile_device?
+      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
+        else
+      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
+        end
+    else
+       if mobile_device?
+      respond_with(resource)
+        else
+      respond_with(resource)
+        end
+    end
+  end
+  
   # GET /resource/confirmation?confirmation_token=abcdef
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
