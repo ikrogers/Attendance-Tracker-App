59f4ce84e863e2dc13feed21af4ee2ce9a16ce44
Ilya Rogers <ikrogers92@gmail.com>
2014-08-05 15:16:52 -0400

    Dynamic dropdowns

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -14,4 +14,29 @@ module GroupsHelper
     return @flag
   end
   
+  def not_assigned_to_attendance_group
+    @attendance = Group.where(grouptype: "Attendance")
+    @users = User.all
+    @users_not_in_attendance = Array.new
+    @allusers = Array.new
+    @attendance.each do |a|
+      @ingroups = InGroup.where(groups_id: a.id)
+      @ingroup.each do |ig|
+        @allusers << ig.users_id
+      end
+    end
+    @allusers.uniq!
+    @users.each do |u|
+      @flag = false
+      @allusers.each do |au|
+        if au != u.id
+          @users_not_in_attendance << u
+          break
+        end
+      end
+    end
+    return @users_not_in_attendance
+    
+  end
+  
 end
