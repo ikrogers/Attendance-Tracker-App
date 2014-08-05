2692415978fbaa7e70c09dc284d2b12097d94e38
Ilya Rogers <ikrogers92@gmail.com>
2014-08-05 03:15:13 -0400

    Confirm

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -8,7 +8,11 @@ class ConfirmationsController < Devise::ConfirmationsController
 
     if resource.errors.empty?
       set_flash_message(:notice, :confirmed) if is_flashing_format?
-      respond_with_navigational(resource){ redirect_to unauthenticated_root_path }
+      if mobile_device?
+          respond_with_navigational(resource){ redirect_to unauthenticated_root_path }
+        else
+          respond_with_navigational(resource){ redirect_to unauthenticated_root_path }
+        end
     else
       respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
     end
