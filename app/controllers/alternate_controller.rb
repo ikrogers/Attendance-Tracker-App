21ceaf42418fc3b9eff862c190dddc2e078cdcfa
Ilya Rogers <ikrogers92@gmail.com>
2014-08-05 21:32:40 -0400

    Excused users added now to add them to groups

--- app/controllers/alternate_controller.rb
+++ app/controllers/alternate_controller.rb
@@ -6,16 +6,82 @@ class AlternateController < ApplicationController
   end
 
   def create
-    @ptdays = params[:project][:ptdays] rescue nil
-    @llabdays = params[:project][:llabdays] rescue nil
+    @ptdays = params[:project][:excused_pt_days] rescue nil
+    @llabdays = params[:project][:excused_llab_days] rescue nil
     @users = User.find(params[:project][:user_ids]) rescue nil
-    @processaction = params[:project][:processaction] rescue nil
+    @processaction = params[:project][:add_remove] rescue nil
+    
+    if @processaction[0] == "Add PT Excuse"
+      if @ptdays != nil
+        @users.each do |user|
+          @ptday_string = ''
+          @ptdays.each do |d|
+            @ptday_string = @ptday_string+'::'+d
+            user.update_attributes(:excused_pt_days => @ptday_string, :ptexcuse => true)
+          end
+        end
+      end
+    elsif @processaction[0] == "Add LLAB Excuse"
+      if @llabdays != nil
 
+        @users.each do |user|
+          @llabday_string = ''
+          @llabdays.each do |d|
+            @llabday_string = @llabday_string+'::'+d
+            user.update_attributes(:excused_llab_days => @llabday_string, :llabexcuse => true)
+          end
+        end
+      end
+    elsif @processaction[0] == "Add LLAB and PT Excuse"
+      if @ptdays != nil
 
+        @users.each do |user|
+          @ptday_string = ''
+          @ptdays.each do |d|
+            @ptday_string = @ptday_string+'::'+d
+            user.update_attributes(:excused_pt_days => @ptday_string, :ptexcuse => true)
+          end
+        end
+      end
+      if @llabdays != nil
+
+        @users.each do |user|
+          @llabday_string = ''
+          @llabdays.each do |d|
+            @llabday_string = @llabday_string+'::'+d
+            user.update_attributes(:excused_llab_days => @llabday_string, :llabexcuse => true)
+          end
+        end
+      end
+    end
+    if @users == nil
+      respond_to do |format|
+      format.html{redirect_to excused_users_path, alert: 'Nothing was updated. You may have forgotten to select some users on the previous page!'}
+    end
+   else
+    respond_to do |format|
+      format.html{redirect_to excused_users_path, notice: 'Users have been successfully added/updated. You may now add them to alternate PT or LLAB groups for attendance tracking!'}
+    end
+end
   end
 
   def index
 
   end
-
+  
+  def remove_pt_excuse
+    @user = User.find_by_id(params[:id])
+    @user.update_attributes(:excused_pt_days => nil, :ptexcuse => false)
+    respond_to do |format|
+      format.html{redirect_to excused_users_path, notice: 'PT excuse removed successfully! User will now appear on all regular attendance tracking forms'}
+    end
+  end
+  
+  def remove_llab_excuse
+    @user = User.find_by_id(params[:id])
+    @user.update_attributes(:excused_llab_days => nil, :llabexcuse => false)
+      respond_to do |format|
+      format.html{redirect_to excused_users_path, notice: 'LLAB excuse removed successfully! User will now appear on all regular attendance tracking forms'}
+    end
+  end
 end
--- app/helpers/alternate_helper.rb
+++ app/helpers/alternate_helper.rb
@@ -1,2 +1,31 @@
 module AlternateHelper
+  def is_today_pt(group)
+    group = Group.find_by_id(group)
+    @availabledays = group.ptdays.split("::")
+    @flag = false
+    @availabledays.each do |a|
+      if Time.now.strftime("%A") == a
+        @flag = true
+      break
+      else
+        @flag = false
+      end
+    end
+    return @flag
+  end
+  
+  def is_today_llab(group)
+    group = Group.find_by_id(group)
+    @availabledays = group.llabdays.split("::")
+    @flag = false
+    @availabledays.each do |a|
+      if Time.now.strftime("%A") == a
+        @flag = true
+      break
+      else
+        @flag = false
+      end
+    end
+    return @flag
+  end
 end
