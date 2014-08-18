151741c3dc25689dfdf5fee8304d4f8aad11ec59
Ilya Rogers <ikrogers92@gmail.com>
2014-08-17 20:36:01 -0400

    Updated mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'example.com',
-  user_name:            'mailertester694@gmail.com',
+  user_name:            'automaticnotification172@gmail.com',
   password:             '78 chickens went on a walk and for got that Google uses CAPTCHA which drives girls up the wall',
   authentication:       'plain',
   enable_starttls_auto: true  }
