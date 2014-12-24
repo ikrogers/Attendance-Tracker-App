5c1a25093cc34a94e1e35dc18a0587cb209fad59
Ilya Rogers <ikrogers92@gmail.com>
2014-12-24 13:07:40 -0500

    Lock out button if there is no events to take attendance for

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -66,5 +66,38 @@ module ApplicationHelper
       return true
     end
   end
+  
+  def assigned_events(group_id)
+    @policy = AttendancePolicy.where(:groups_id => group_id)
+    @policy_all = AttendancePolicy.where(:groups_id => nil)
+
+    @events = Array.new
+    @allowed = Array.new
+    @policy.each do |p|
+      @events << p.event
+    end
+    @policy_all.each do |p|
+      @events << p.event
+    end
+    @events.uniq().each do |e|
+      @e = Event.find_by_event_name(e)
+      if @e.event_days != nil
+        @e.event_days.each do |ed|
+          if Time.now.strftime("%A") == ed
+            @flag = true
+          break
+          else
+            @flag = false
+          end
+        end
+        if @flag == true
+        @allowed << @e.event_name
+        end
+
+      end
+    end
+    return @allowed
+  end
+
 
 end
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,35 +1,12 @@
 module AttendancesHelper
-  def assigned_events(group_id)
-    @policy = AttendancePolicy.where(:groups_id => group_id)
-    @policy_all = AttendancePolicy.where(:groups_id => nil)
-
-    @events = Array.new
-    @allowed = Array.new
-    @policy.each do |p|
-      @events << p.event
-    end
-    @policy_all.each do |p|
-      @events << p.event
-    end
-
-    @events.uniq().each do |e|
-      @e = Event.find_by_event_name(e)
-      if @e.event_days != nil
-        @e.event_days.each do |ed|
-          if Time.now.strftime("%A") == ed
-            @flag = true
-          break
-          else
-            @flag = false
-          end
-        end
-        if @flag == true
-        @allowed << @e.event_name
-        end
-
+  def users_in_group(group)
+  @group=group
+      @ingroup = InGroup.where(groups_id: @group.id)      
+      @usersingroup= Array.new
+      @ingroup.each do |ig|
+        @usersingroup << User.find_by_id(ig.users_id)
       end
-    end
-    return @allowed
+  
+  return @usersingroup
   end
-
 end
