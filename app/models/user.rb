f1d6fc85aca8872d73702a3d4771ba28625f6aaa
Ilya Rogers <ikrogers92@gmail.com>
2015-01-06 09:01:42 -0700

    removed confirmabale added script to pop users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,28 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :session_limitable, :confirmable
+         :recoverable, :rememberable, :trackable, :session_limitable
          
   has_many :in_group
   
 
-  has_many :messages 
-  validates :fname, :presence => true
-  validates :lname, :presence => true
-  validates_uniqueness_of :email, :scope => [:email]
-  validates :email, :presence => true,
-                    :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :message => "is in invalid format" } 
-  validates :phone, :format => { :with => /\A(\+1)?[0-9]{10}\z/, :message => "not a valid 10-digit telephone number" } 
-  validates :carrier, :presence => true
-  validates :password, :presence => true,
-                       :confirmation => true,
-                       :length => {:within => 6..40},
-                       :on => :create
-  validates :password, :confirmation => true,
-                       :length => {:within => 6..40},
-                       :allow_blank => true,
-                       :on => :update
-
+  
 
   
   
