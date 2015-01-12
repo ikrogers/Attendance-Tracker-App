e730227836327dfc82a7c321484ff1c9395f8982
Ilya Rogers <ikrogers92@gmail.com>
2015-01-12 13:02:14 -0500

    Test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(:email => "ikrogers@valdosta.edu",:fname=>"Ilya", :lname=>"Rogers", :phone => "9125802665", :carrier=>"Verizon", :password => "123123123", :password_confirmation => "123123123", :admin => true, :uberadmin => true)
-User.create(:email => "ikrogers2@valdosta.edu",:fname=>"Ilya", :lname=>"Rogers", :phone => "9125802665", :carrier=>"Verizon", :password => "123123123", :password_confirmation => "123123123", :admin => true, :uberadmin => true)
-User.create(:email => "ikrogers3@valdosta.edu",:fname=>"Ilya", :lname=>"Rogers", :phone => "9125802665", :carrier=>"Verizon", :password => "123123123", :password_confirmation => "123123123", :admin => true, :uberadmin => true)
+User.create(:email => "ikrogers@valdosta.edu",:fname=>"Ilya", :lname=>"Rogers", :phone => "9125802665", :carrier=>"Verizon", :password => "123123123", :confirmed_at => '2015-01-10 17:14:57.899414', :password_confirmation => "123123123", :admin => true, :uberadmin => true)
+User.create(:email => "ikrogers2@valdosta.edu",:fname=>"Ilya", :lname=>"Rogers", :phone => "9125802665", :carrier=>"Verizon", :password => "123123123", :confirmed_at => '2015-01-10 17:14:57.899414', :password_confirmation => "123123123", :admin => true, :uberadmin => true)
+User.create(:email => "ikrogers3@valdosta.edu",:fname=>"Ilya", :lname=>"Rogers", :phone => "9125802665", :carrier=>"Verizon", :password => "123123123", :confirmed_at => '2015-01-10 17:14:57.899414', :password_confirmation => "123123123", :admin => true, :uberadmin => true)
