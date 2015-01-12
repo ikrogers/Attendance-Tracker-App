00b67a5afa3ab1e51bc63d6e221c8e90c1a69068
Ilya Rogers <ikrogers92@gmail.com>
2015-01-12 14:30:25 -0500

    Added table in attendance form that shows recordes for today

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'example.com',
+  domain:               'gmail.com',
   user_name:            'hurrdurr123333333@gmail.com',
   password:             '123123123123123123',
   authentication:       'plain',
